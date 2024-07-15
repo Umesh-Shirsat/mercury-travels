c4245284f5857c302f988c0cbf93f8831165f293
Umesh-Shirsat <Umesh.Shirsat@winjit.com>
2024-07-15 10:28:25 +0530

    test commit

--- cypress/support/step_definitions/mercury-travels.js
+++ cypress/support/step_definitions/mercury-travels.js
@@ -37,8 +37,8 @@ Then("User gets error of already registered", () => {
     "body > div:nth-child(10) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > h3:nth-child(2)"
   ).should("have.text", "Registration Failed");
 });
-
 //*********** */
+
 // When("User clicks on register", () => {
 //   cy.get(
 //     "li[class='dropdown loggedout open'] li:nth-child(2) a:nth-child(1)"
@@ -58,6 +58,7 @@ Then("User gets error of already registered", () => {
 //   ).click();
 // });
 
+//******************** */
 When("User logs in", (table) => {
   homepage.login(table);
 });
@@ -66,3 +67,101 @@ When("User logs in", (table) => {
 //   const welcome = await homepage.getLogin().text();
 //   expect(welcome.includes("world"));
 // });
+//********************** */
+
+// Check for about us dropdown
+When("User clicks on about us tab", () => {
+  homepage.getLogin().eq(3).click();
+});
+
+// Check for about us page
+When("User clicks on about us option in dropdown", () => {
+  cy.get(
+    "body > nav:nth-child(4) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(10) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)"
+  ).click();
+});
+
+Then("User should visit about us page", () => {
+  cy.get(".col-md-12.tittle.font30").should("have.text", "About Us");
+});
+
+// Check for branch page
+When("User clicks on Find a Branch option in dropdown", () => {
+  cy.get(
+    "body > nav:nth-child(4) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(10) > ul:nth-child(2) > li:nth-child(2) > a:nth-child(1)"
+  ).click();
+});
+
+Then("User should visit branch page", () => {
+  cy.get(".tittle").should("have.text", "Our Branches");
+});
+
+// Check for testemonials page
+When("User clicks on Testimonials option in dropdown", () => {
+  cy.get(
+    "body > nav:nth-child(4) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(10) > ul:nth-child(2) > li:nth-child(3) > a:nth-child(1)"
+  ).click();
+});
+
+Then("User should visit Testimonials page", () => {
+  cy.get(".col-md-12.tittle.font30").should("have.text", "Testimonials");
+});
+
+// Check for awards page
+When("User clicks on Awards option in dropdown", () => {
+  cy.get(
+    "body > nav:nth-child(4) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(10) > ul:nth-child(2) > li:nth-child(4) > a:nth-child(1)"
+  ).click();
+});
+
+Then("User should visit Awards page", () => {
+  cy.get(".col-md-12.tittle.font30").should("have.text", "Awards");
+});
+
+// Check for careers page
+When("User clicks on Careers option in dropdown", () => {
+  cy.get(
+    "body > nav:nth-child(4) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(10) > ul:nth-child(2) > li:nth-child(5) > a:nth-child(1)"
+  ).click();
+});
+
+Then("User should visit Careers page", () => {
+  cy.get(".col-md-12.tittle.font30").should("have.text", "Careers");
+});
+
+// Check for contact us page
+When("User clicks on Contact Us option in dropdown", () => {
+  cy.get(
+    "body > nav:nth-child(4) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(10) > ul:nth-child(2) > li:nth-child(6) > a:nth-child(1)"
+  ).click();
+});
+
+Then("User should visit Contact Us page", () => {
+  cy.get(".col-md-12.tittle.font30").should("have.text", "Contact Us");
+});
+//********************************** */
+
+// Check for insurance page
+When("User clicks on Insurance tab", () => {
+  cy.get(".nav:nth-child(1) > li:nth-child(9)").click();
+});
+
+// Check for forex page
+When("User clicks on Forex tab", () => {
+  cy.get(".nav:nth-child(1) > li:nth-child(8) > a")
+    .invoke("removeAttr", "target")
+    .click();
+});
+
+Then("User should visit Insurance page", () => {
+  cy.origin("https://buyforex.com", () => {
+    cy.get(".modal-title.modal-title.country-hdr").should(
+      "have.text",
+      "Select your City"
+    );
+  });
+});
+
+Then("User should see 7 scrolling images", () => {
+  cy.get(".int_banner2").should("have.lengthOf", 7);
+});
